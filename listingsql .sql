e7418a911d280fc2e145773bc6b147595018f967
ANANAzZzZz <vladscooltrol@gmail.com>
2023-10-14 22:38:13 +0300

    initial commit

---
+++ listingsql .sql
@@ -0,0 +1,97 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS Courier;
+DROP TABLE IF EXISTS Order_info;
+DROP TABLE IF EXISTS Boardgame_in_circulation;
+DROP TABLE IF EXISTS Owner;
+DROP TABLE IF EXISTS Renter;
+DROP TABLE IF EXISTS Boardgame;
+
+CREATE TABLE Boardgame( 
+"ID" INTEGER PRIMARY KEY,
+"Status" VARCHAR(30),
+"Name" VARCHAR(30),
+"Description" VARCHAR(30),
+"Middle_game_time" TIME,
+"Min_players" INTEGER,
+"Max_players" INTEGER,
+"Age" INTEGER,
+"Rools" VARCHAR(30),
+"Image" VARCHAR(30),
+"Rating" FLOAT,
+"Price_per_day" MONEY,
+"Base_cost" MONEY,
+"Complexity" FLOAT,
+"Category" VARCHAR(30));
+
+CREATE TABLE Renter(
+"FIO" VARCHAR(30),
+"ID" INTEGER PRIMARY KEY,
+"Rating" FLOAT);
+
+CREATE TABLE Owner(
+"FIO" VARCHAR(30),
+"ID" INTEGER PRIMARY KEY,
+"Rating" FLOAT);
+
+CREATE TABLE Boardgame_in_circulation(
+"ID_Renter" INTEGER REFERENCES Renter("ID") ON DELETE CASCADE ON UPDATE CASCADE,
+"ID_boardgame" INTEGER REFERENCES Boardgame("ID") ON DELETE CASCADE ON UPDATE CASCADE,
+"ID_Owner" INTEGER REFERENCES Owner("ID")ON DELETE CASCADE ON UPDATE CASCADE,
+"Status_boardgame" VARCHAR(30),
+"Boardgame_state" VARCHAR(30),
+PRIMARY KEY("ID_renter","ID_boardgame","ID_Owner"));
+
+CREATE TABLE Order_info(
+"ID" INTEGER PRIMARY KEY,
+"Status" VARCHAR(30),
+"Order_time" Time,
+"Addres_recive" VARCHAR(30),
+"Addres_send"VARCHAR(30),
+"ID_renter" INTEGER, 
+"ID_boardgame" INTEGER, 
+"ID_owner" INTEGER,
+"ID_courier" INTEGER REFERENCES Courier("ID")ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY("ID_renter", "ID_boardgame", "ID_owner") REFERENCES Boardgame_in_circulation("ID_Renter", "ID_boardgame", "ID_Owner") ON DELETE CASCADE ON UPDATE CASCADE);
+
+CREATE TABLE Courier(
+"FIO" VARCHAR(30),
+"ID" INTEGER PRIMARY KEY,
+"Rating" FLOAT);
+
+INSERT INTO Courier("FIO", "ID","Rating") VALUES 
+('ГУАП',1,2.2),
+('ИТМО',2,3.3);
+
+INSERT INTO Boardgame(
+"ID", 
+"Status",
+"Name",
+"Description",
+"Middle_game_time",
+"Min_players",
+"Max_players",
+"Age",
+"Rools",
+"Image",
+"Rating",
+"Price_per_day",
+"Base_cost",
+"Complexity",
+"Category") VALUES (
+    0,
+    "Доступна",
+    "битва бомжей",
+    "боевая стратегия",
+    "00:10:00",
+    2,
+    6,
+    18,
+    "бить бомжей",
+    "url", 
+    "4.85",
+    50,
+    500,
+    "сложно",
+    "стратегия"
+    );
---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+import sqlite3
+import json
+
+app = Flask(__name__)
+
+# db
+connection = sqlite3.connect("db.db")
+cursor = connection.cursor()
+
+cursor.execute('SELECT * FROM Boardgame')
+allBoardgames = cursor.fetchall()
+connection.close()
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+allBoardgames_list = []
+
+def find_all_boardgames():
+    for boardgame in allBoardgames:
+        print(boardgame)
+        allBoardgames_dict = {
+            'ID': boardgame[0],
+            'Status': boardgame[1],
+            'Name': boardgame[2],
+            'Description': boardgame[3],
+            'Middle_game_time': boardgame[4],
+            'Min_players': boardgame[5],
+            'Max_players': boardgame[6],
+            'Age': boardgame[7],
+            'Rools': boardgame[8],
+            'Image': boardgame[9],
+            'Rating': boardgame[10],
+            'Price_per_day': boardgame[11],
+            'Base_cost': boardgame[12],
+            'Complexity': boardgame[13],
+            'Category': boardgame[14],
+        }
+
+        allBoardgames_list.append(allBoardgames_dict)
+
+        return allBoardgames_list
+
+@app.route('/boardGames')
+def loadMenu():
+    return jsonify(find_all_boardgames())
+
+if __name__ == "__main__":
+    app.run()
