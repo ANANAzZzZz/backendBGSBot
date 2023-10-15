0776ddff42b8d0050a0b4a44240f91220b714e0b
ANANAzZzZz <vladscooltrol@gmail.com>
2023-10-15 04:28:59 +0300

    add cors

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from flask import render_template
 from flask import jsonify
 import sqlite3
 import json
-from  flask import request
+from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
 
-count = 0
 def find_all_boardgames():
     # db
     connection = sqlite3.connect("db.db")
